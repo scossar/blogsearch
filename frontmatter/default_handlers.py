1a318b8d241bb70796b21269039e275731548478
Simon Cossar <scossar3@gmail.com>
2025-11-30 20:54:37 -0800

    changing toml libraries

--- frontmatter/default_handlers.py
+++ frontmatter/default_handlers.py
@@ -1,26 +1,42 @@
+"""
+This module includes the handlers for YAML and TOML and a BaseHandler that outlines
+the API.
+
+A handler needs to do four things:
+- detect if it can parse the text
+- split the frontmatter from the content, returning both as a tuple
+- parse the frontmatter into a dictionary
+- export a dictionary back into text
+"""
+
+# To deal with forward referencing, but I'm not sure I've got this sorted out
+# the __future__ import postpones the evaluation of type annotations in a module until
+# runtime. The conflict is that Post uses the BaseHandler type and BaseHandler uses the Post type
 from __future__ import annotations
 
 import re
+import tomllib as toml  # tomllib is in the Python standard library as of 3.11
+import tomli_w  # used because tomllib doesn't have a dump method
 import yaml
 from .util import u
 from .post import Post
 
-from typing import Any, Type, Iterable
+from typing import Any, Type
 
+# For my case this could probably be simplified to
+# from yaml import CSafeDumper as SafeDumper
+# from yaml import CSafeLoader as SafeLoader
+# The try block could be removed.
 SafeDumper: Type[yaml.CDumper] | Type[yaml.SafeDumper]
 SafeLoader: Type[yaml.CSafeLoader] | Type[yaml.SafeLoader]
 
+# Handle the case of PyYAML having been installed without C extensions
 try:
     from yaml import CSafeDumper as SafeDumper
     from yaml import CSafeLoader as SafeLoader
 except ImportError:
     from yaml import SafeDumper
     from yaml import SafeLoader
-
-try:
-    import toml
-except ImportError:
-    toml = None
 
 __all__ = ["BaseHandler", "YAMLHandler", "TOMLHandler"]
 
@@ -35,7 +51,9 @@ DEFAULT_POST_TEMPLATE = """\
 
 
 class BaseHandler:
-    FM_BOUNDARY: re.Pattern[str] | None = None
+    FM_BOUNDARY: re.Pattern[str] | None = (
+        None  # the type is the re | None union; the default is the = None part
+    )
     START_DELIMITER: str | None = None
     END_DELIMITER: str | None = None
 
@@ -58,23 +76,38 @@ class BaseHandler:
             )
 
     def detect(self, text: str) -> bool:
+        """
+        Detect if the handler can parse the given text.
+        """
         assert self.FM_BOUNDARY is not None
         if self.FM_BOUNDARY.match(text):
             return True
         return False
 
     def split(self, text: str) -> tuple[str, str]:
+        """
+        Split text into frontmatter and content.
+        """
         assert self.FM_BOUNDARY is not None
         _, fm, content = self.FM_BOUNDARY.split(text, 2)
         return fm, content
 
     def load(self, fm: str) -> dict[str, Any]:
+        """
+        Parse frontmatter and return a dict.
+        """
         raise NotImplementedError
 
     def export(self, metadata: dict[str, object], **kwargs: object) -> str:
+        """
+        Turn metadata back into text.
+        """
         raise NotImplementedError
 
     def format(self, post: Post, **kwargs: object) -> str:
+        """
+        Turn a post into a string, used in `frontmatter.dumps`.
+        """
         start_delimiter = kwargs.pop("start_delimiter", self.START_DELIMITER)
         end_delimiter = kwargs.pop("end_delimiter", self.END_DELIMITER)
 
@@ -89,14 +122,24 @@ class BaseHandler:
 
 
 class YAMLHandler(BaseHandler):
+    """
+    Load and export YAML metadata.
+    """
+
     FM_BOUNDARY = re.compile(r"^-{3,}\s*$", re.MULTILINE)
     START_DELIMITER = END_DELIMITER = "---"
 
     def load(self, fm: str, **kwargs: object) -> Any:
+        """
+        Parse YAML frontmatter.
+        """
         kwargs.setdefault("Loader", SafeLoader)
         return yaml.load(fm, **kwargs)  # type: ignore[arg-type]
 
     def export(self, metadata: dict[str, object], **kwargs: object) -> str:
+        """
+        Export metadata as YAML.
+        """
         kwargs.setdefault("Dumper", SafeDumper)
         kwargs.setdefault("default_flow_style", False)
         kwargs.setdefault("allow_unicode", True)
@@ -106,6 +149,12 @@ class YAMLHandler(BaseHandler):
 
 
 class TOMLHandler(BaseHandler):  # pyright: ignore
+    """
+    Load and export TOML metadata.
+
+    By default, the split is based on `+++`
+    """
+
     FM_BOUNDARY = re.compile(r"^\+{3,}\s*$", re.MULTILINE)
     START_DELIMITER = END_DELIMITER = "+++"
 
@@ -114,6 +163,11 @@ class TOMLHandler(BaseHandler):  # pyright: ignore
         return toml.loads(fm, **kwargs)  # pyright: ignore
 
     def export(self, metadata: dict[str, object], **kwargs: object) -> str:
+        "Turn metadata into TOML"
         assert toml is not None
-        metadata_str = toml.dumps(metadata)
+        # Serialize Python data back into TOML. `tomllib` doesn't have a
+        # `dumps` method, so...
+        # This seems to be adding a newline at the end of the metadata
+        metadata_str = tomli_w.dumps(metadata)
+        print("TOML\n", metadata_str)
         return u(metadata_str)
